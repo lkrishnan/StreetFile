7ce8096f6c2d68b0656f6c91075c36751d212bc5
Krishnan <Lakshmanan.Krishnan@mecklenburgcountync.gov>
2021-04-15 10:10:35 -0400

    Changes thus far

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<MainToolbar />
+		<v-main class="grey lighten-4">
+			<Login />
+			<Information />
+			<Edit />
+		</v-main>
+		<v-snackbar bottom right :value="updateExists" :timeout="-1" color="primary">
+    		An update is available
+    		<v-btn text @click="refreshApp">Update</v-btn>
+  		</v-snackbar>
+  	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import MainToolbar from "./components/MainToolbar.vue";
+  	import Login from "./components/Login.vue";
+  	import Information from "./components/Information.vue";
+  	import Edit from "./components/Edit.vue";
+	
+  	export default {
+    	name: "App",
+    	
+		components: {
+      		MainToolbar,
+      		Login, 
+      		Information,
+      		Edit
+    	},
+  		
+		data( ){
+			return {
+				// refresh variables
+				refreshing: false,
+				registration: null,
+				updateExists: false,
+			}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    	},
+  	    
+		created( ){
+			// Listen for our custom event from the SW registration
+			document.addEventListener( 'swUpdated', this.updateAvailable, { once: true } )
+			
+			// Prevent multiple refreshes
+			if( "serviceWorker" in navigator ){
+				navigator.serviceWorker.addEventListener( 'controllerchange', ( ) => {
+					if( this.refreshing ) 
+						return
+					this.refreshing = true
+					// Here the actual reload of the page occurs
+					window.location.reload( )
+			
+				} )
+			}
+
+    	},
+  
+		methods: {
+			// Store the SW registration so we can send it a message
+			// We use `updateExists` to control whatever alert, toast, dialog, etc we want to use
+			// To alert the user there is an update they need to refresh for
+			updateAvailable( event ){
+				this.registration = event.detail
+				this.updateExists = true
+			
+			},
+		
+			// Called when the user accepts the update
+			refreshApp( ){
+				this.updateExists = false
+
+				// Make sure we only send a 'skip waiting' message if the SW is waiting
+				if( !this.registration || !this.registration.waiting ) 
+					return
+				// send message to SW to skip the waiting and activate the new SW
+				this.registration.waiting.postMessage( { type: 'SKIP_WAITING' } )
+
+			}
+
+		}
+
+  	};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+import './main.css'
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
+import vuetify from './plugins/vuetify';
+//import "core-js/stable";
+//import "regenerator-runtime/runtime";
 
 Vue.config.productionTip = false
 
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+new Vue( {
+  	store,
+  	vuetify,
+	render: h => h( App )
+} ).$mount( "#app" )
